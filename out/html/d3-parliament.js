eeb6596e849b1511a87ca366d48541ecfc2dbf01
Grey <pjmyers2007@gmail.com>
2026-06-21 23:05:53 -0400

    test 5

--- out/html/d3-parliament.js
+++ out/html/d3-parliament.js
@@ -43,11 +43,11 @@ d3.parliament = function() {
 
             /***
              * compute number of seats and rows of the parliament */
-            var nSeats = 0;
+            var nSeats = 767;
             d.forEach(function(p) { nSeats += (typeof p.seats === 'number') ? Math.floor(p.seats) : p.seats.length; });
 
-            var nRows = 15;
-            var maxSeatNumber = 767;
+            var nRows = 767;
+            var maxSeatNumber = 15;
             var b = 0.5;
             (function() {
                 var a = innerRadiusCoef / (1 - innerRadiusCoef);
